b5e3c9aef9a523cd52ca33666d0b03461da09bbc
lorang92 <steffen.ekeberg@soprasteria.com>
2019-10-05 17:53:35 +0200

    new model

--- Tailspin.SpaceGame.Web.Models/Models/Model.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Model.cs
@@ -10,5 +10,9 @@ namespace TailSpin.SpaceGame.Web.Models
         // The value that uniquely identifies this object.
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
+        // The name of the model
+        [JsonProperty(PropertyName = "name")]
+        public string Name {get; set; }
     }
 }
